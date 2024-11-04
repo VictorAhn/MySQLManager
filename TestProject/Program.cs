b8448b3c991752494a2466e5d92680ef4a98a69c
현수 안 <sj99119@naver.com>
2024-11-04 11:11:03 +0900

    테스트용 샘플 코드 작성

---
+++ TestProject/Program.cs
@@ -0,0 +1,49 @@
+﻿using MySQLManager;
+
+namespace TestProject
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            bool insert = await Insert("UID", "UNM");
+            string userName = await Select("UID");
+            bool update = await Update("UID", "UNM");
+        }
+        public static async Task<bool> Insert(string userId, string userName)
+        {
+            try
+            {
+                string xmlPath = Path.Combine("Test.xml");
+
+                var query = await MySqlDapperManager.GetQueryFirstFromXmlAsync<bool>(ConnectionStringEnum.Default, xmlPath, "Insert", new { userId = userId, userName = userName });
+                return true;
+            }
+            catch (Exception exception) { }
+            return false;
+        }
+        public static async Task<string> Select(string userId)
+        {
+            try
+            {
+                string xmlPath = Path.Combine("Test.xml");
+
+                return await MySqlDapperManager.GetQueryFirstFromXmlAsync<string>(ConnectionStringEnum.Default, xmlPath, "Select", new { userId = userId });
+            }
+            catch (Exception exception) { }
+            return null;
+        }
+        public static async Task<bool> Update(string userId, string userName)
+        {
+            try
+            {
+                string xmlPath = Path.Combine("Test.xml");
+
+                var query = await MySqlDapperManager.GetQueryFirstFromXmlAsync<bool>(ConnectionStringEnum.Default, xmlPath, "Update", new { userId = userId, userName = userName });
+                return true;
+            }
+            catch (Exception exception) { }
+            return false;
+        }
+    }
+}
